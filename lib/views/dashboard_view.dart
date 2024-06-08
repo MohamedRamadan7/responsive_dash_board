1abc6f790021662688e0e0fc4fd75d5c3d71948a
MohamedRamadan7 <mohamedramadan6420@gmail.com>
2024-06-08 16:19:04 +0300

    AppStyles

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/views/widgets/Dashboard_desktop_layout.dart';
+import 'package:responsive_dash_board/views/widgets/adaptive_layout.dart';
 
 class DashboardView extends StatelessWidget {
   const DashboardView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: AdaptiveLayout(
+          mobilelayout: (context) => const SizedBox(),
+          tabletLayout: (context) => const SizedBox(),
+          desktopLayout: (context) => const DashboardDesktopLayout()),
+    );
   }
 }
