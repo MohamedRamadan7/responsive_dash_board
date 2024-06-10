f647658582d8c1fa971108afed66e0986bb778a4
MohamedRamadan7 <mohamedramadan6420@gmail.com>
2024-06-10 14:49:52 +0300

    CustomBackgroundContainer

--- lib/views/widgets/all_expenses.dart
+++ lib/views/widgets/all_expenses.dart
@@ -2,21 +2,15 @@ import 'package:flutter/material.dart';
 
 import 'all_expenses_header.dart';
 import 'all_expenses_item_list_view.dart';
+import 'custom_background_container.dart';
 
 class AllExpenses extends StatelessWidget {
   const AllExpenses({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      decoration: ShapeDecoration(
-        color: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
-        ),
-      ),
-      child: const Column(
+    return const CustomBackgroundContainer(
+      child: Column(
         children: [
           AllExpensesHeader(),
           SizedBox(height: 16),
@@ -26,3 +20,4 @@ class AllExpenses extends StatelessWidget {
     );
   }
 }
+
